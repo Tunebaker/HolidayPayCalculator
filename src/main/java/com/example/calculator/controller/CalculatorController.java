5c198269d5d9280593e6bf19ad189ab9ce43985b
Tunebaker <tunebaker@gmail.com>
2023-05-08 13:07:53 +0300

    change double to Bigdecimal for salary

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -8,19 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/calculate")
 public class CalculatorController {
     @GetMapping(params = {"salary", "duration"} )
-    public double calculatePay(@RequestParam(value = "salary") @Min(0) double avgMountlySalary,
+    public BigDecimal calculatePay(@RequestParam(value = "salary") @Min(0) BigDecimal avgMountlySalary,
                                @RequestParam(value = "vacationDuration") @Min(1) int vacationDuration) {
         return Calculator.calculateByDuration(avgMountlySalary, vacationDuration);
     }
 
     @GetMapping(params = {"salary", "vacationStart", "vacationEnd"} )
-    public double calculatePay(@RequestParam(value = "salary") @Min(0) double avgMountlySalary,
+    public BigDecimal calculatePay(@RequestParam(value = "salary") @Min(0) BigDecimal avgMountlySalary,
                                @RequestParam(value = "vacationStart")  @Valid LocalDate vacationStartDate,
                                @RequestParam(value = "vacationEnd")  @Valid LocalDate vacationEndDate) {
 
--- src/main/java/com/example/calculator/service/Calculator.java
+++ src/main/java/com/example/calculator/service/Calculator.java
@@ -4,6 +4,8 @@ import com.example.calculator.data.HolidayDatesReader;
 import com.example.calculator.data.HolidaysDatesXmlReader;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
@@ -11,13 +13,13 @@ import java.util.stream.Collectors;
 
 @Component
 public class Calculator {
-    private static final double AVERAGE_MONTHLY_NUMBER_OF_DAYS = 29.3;
+    private static final BigDecimal AVERAGE_MONTHLY_NUMBER_OF_DAYS = BigDecimal.valueOf(29.3);
 
-    public static double calculateByDuration(double avgMountlySalary, int duration) {
-        return avgMountlySalary / AVERAGE_MONTHLY_NUMBER_OF_DAYS * duration;
+    public static BigDecimal calculateByDuration(BigDecimal avgMountlySalary, int duration) {
+        return avgMountlySalary.divide(AVERAGE_MONTHLY_NUMBER_OF_DAYS, 3, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(duration));
     }
 
-    public static double calculateByVacationDates(double avgMontlySalary, LocalDate vacationStart, LocalDate vacationEnd) {
+    public static BigDecimal calculateByVacationDates(BigDecimal avgMontlySalary, LocalDate vacationStart, LocalDate vacationEnd) {
         List<LocalDate> vacationDates = vacationStart.datesUntil(vacationEnd.plusDays(1))
                 .collect(Collectors.toList());
         int paidDays = vacationDates.size() - getNonWorkingDaysNumber(vacationDates);
