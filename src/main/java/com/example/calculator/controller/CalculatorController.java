e2d663aeabfa621e6d44be3276b4bdda47697b70
Tunebaker <tunebaker@gmail.com>
2023-05-08 13:16:51 +0300

    fix Bigdecimal validation bug

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package com.example.calculator.controller;
 
 import com.example.calculator.service.Calculator;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,22 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/calculate")
+@Validated
 public class CalculatorController {
-    @GetMapping(params = {"salary", "duration"} )
-    public BigDecimal calculatePay(@RequestParam(value = "salary") @Min(0) BigDecimal avgMountlySalary,
-                               @RequestParam(value = "vacationDuration") @Min(1) int vacationDuration) {
+    @GetMapping(params = {"salary", "duration"})
+    public BigDecimal calculatePay(@RequestParam(value = "salary") @PositiveOrZero BigDecimal avgMountlySalary,
+                                   @RequestParam(value = "vacationDuration") @Min(1) int vacationDuration) {
         return Calculator.calculateByDuration(avgMountlySalary, vacationDuration);
     }
 
-    @GetMapping(params = {"salary", "vacationStart", "vacationEnd"} )
-    public BigDecimal calculatePay(@RequestParam(value = "salary") @Min(0) BigDecimal avgMountlySalary,
-                               @RequestParam(value = "vacationStart")  @Valid LocalDate vacationStartDate,
-                               @RequestParam(value = "vacationEnd")  @Valid LocalDate vacationEndDate) {
+    @GetMapping(params = {"salary", "vacationStart", "vacationEnd"})
+    public BigDecimal calculatePay(@RequestParam(value = "salary") @PositiveOrZero BigDecimal avgMountlySalary,
+                                   @RequestParam(value = "vacationStart") @Valid LocalDate vacationStartDate,
+                                   @RequestParam(value = "vacationEnd") @Valid LocalDate vacationEndDate) {
 
         return Calculator.calculateByVacationDates(avgMountlySalary, vacationStartDate, vacationEndDate);
     }
