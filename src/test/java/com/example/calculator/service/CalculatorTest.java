1b47429a5604b8256414f69e219051e1e62eb7a3
Tunebaker <tunebaker@gmail.com>
2023-05-09 20:54:34 +0300

    fix tests

--- src/test/java/com/example/calculator/service/CalculatorTest.java
+++ src/test/java/com/example/calculator/service/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.example.calculator.service;
 
+import com.example.calculator.data.HolidayDatesXmlReader;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
@@ -7,25 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class CalculatorTest {
+    Calculator calculator = new Calculator(new HolidayDatesXmlReader());
 
     @Test
     void calculateByDuration() {
-        BigDecimal calculated = Calculator.calculateByDuration(BigDecimal.valueOf(29.3), 1000);
+        BigDecimal calculated = calculator.calculateByDuration(BigDecimal.valueOf(29.3), 1000);
         BigDecimal expected = BigDecimal.valueOf(1000);
         assertEquals(0, expected.compareTo(calculated));
 
-        calculated = Calculator.calculateByDuration(BigDecimal.valueOf(0), 1);
+        calculated = calculator.calculateByDuration(BigDecimal.valueOf(0), 1);
         expected = BigDecimal.valueOf(0);
         assertEquals(0, expected.compareTo(calculated));
 
-        calculated = Calculator.calculateByDuration(BigDecimal.valueOf(1_000_000), 10_000_000);
+        calculated = calculator.calculateByDuration(BigDecimal.valueOf(1_000_000), 10_000_000);
         expected = BigDecimal.valueOf(341296928327.645);
         assertEquals(0, expected.compareTo(calculated));
     }
 
-
+   /*
     @Test
     void calculateByVacationDates() {
 
-    }
+    }*/
 }
